39146ebf1375f64d355706b769dcc9b461c8710e
taha <taha.askari98989@gmail.com>
2026-06-03 18:41:58 +0330

    Update and rename defictions.py to defictions_En.py

--- defictions_En.py
+++ defictions_En.py
@@ -1,8 +1,11 @@
 def split(text):
-    l1 = text.split()
-    return l1
+    text = text.lower()
+    output_text1 = text.split()
+    return output_text1
 
-def last_points(text):
+def find(text):
+
+    global txt
 
     verbs_for_imprative = ["can", "go", "come", "take", "put", "eat", "drink", "sit", "stand",
          "close", "open", "look", "listen", "wait", "stop", "run", "write",
@@ -15,88 +18,108 @@ def last_points(text):
 
     n = len(text)
 
-    if "hello" in text or "hello!" in text or "hello !"in text or "hello  !" in text:   #when sentense in only 'hello'
+    if "hello" in text or "hello!" in text or "hello !"in text or "hello  !" in text:
+
+        if txt.lower() == "hello" or txt.lower() == "hello!" or txt.lower() == "hello !" or txt.lower() == "hello  !" or txt.lower() == " hello " or txt.lower() == " hello!":
+
+            return "simple(ساده)"
 
         if "?" in text[n - 1]:
 
             if text[0] == "is" or text[0] == "are" or text[0] == "am":
             
-                return "asking"
+                return "asking(پرسشي)"
             
             elif "what" in text or "why" in text:
 
-                return "asking"
+                return "asking(پرسشی)"
             
             elif text[0] in verbs_for_imprative:
                 
-                return "imperative"
+                return "imperative(امري)"
             
             else:
-                return "asking"
+                return "asking(پرسشي)"
         
         elif "!" in text[n - 1]:
 
+            for i in verbs_for_emitional:
+
+                if i in text:
+                    
+                    return "emitional(عاطفی)"
+                #
+        
             if "your" in text or "he" in text or "she" in text or "my" in text or "it" in text or "they" in text or "we" in text or "our" in text or "their" in text:
+                
+                return "declarative(خبري)"
             
-                return "declarative"
+            elif "yours" in text or "mine" in text or "his" in text or "hers" in text or "its" in text:
 
-            elif verbs_for_emitional in text:
-
-                return "Emotional"
+                return "declarative(خبری)"
 
             else:
-                return "exclamatory"
 
+                return "exclamatory(تعجبي)"
+        
                 
         else:
-            
 
-            return "simple"
+            return "simple(ساده)"
 
     elif "?" in text[n - 1]:   # when question mark in sentence(text):
         
         if text[0] == "is" or text[0] == "are" or text[0] == "am":
             
-            return "asking"
+            return "asking(پرسشي)"
         elif "what" in text or "why" in text:
 
-            return "asking"
+            return "asking(پرسشی)"
         
         elif text[0] in verbs_for_imprative:
             
-            return "imperative"
+            return "imperative(امري)"
         
         else:
-            return "asking"
+            return "asking(پرسشي)"
         
     elif "!" in text[n - 1]:   # when Exclamation mark in sentence(text):
         
-        if "your" in text or "he" in text or "she" in text or "my" in text or "it" in text or "they" in text or "we" in text or "our" in text or "their" in text:
-            
-            return "declarative"
+        for i in verbs_for_emitional:
 
-        elif "love" in text or "hate" in text or "happy" in text or "sad" in text or "angry" in text or "excited" in text or "scare" in text or "scared" in text or "tired" in text or "bored" in text or "beautiful" in text:
+            if i in text:
 
-            return "Emotional"
-
-        else:
-            return "exclamatory"
-    elif "." in text[n - 1]:   #when sentence has only ' . ' in last
+                return "emitional(عاطفی)"
+            #
         
         if "your" in text or "he" in text or "she" in text or "my" in text or "it" in text or "they" in text or "we" in text or "our" in text or "their" in text:
             
-            return "declarative"
+            return "declarative(خبري)"
+
+
+        else:
+            return "exclamatory(تعجبي)"
+        
+    elif "." in text[n - 1]:
+        
+        if "your" in text or "he" in text or "she" in text or "my" in text or "it" in text or "they" in text or "we" in text or "our" in text or "their" in text:
+            
+            return "declarative(خبري)"
+        
     elif text[0] == "can" or text[0] == "is" or text[0] == "are" or text[0] == "am":
 
-        return "asking"
+        return "asking(پرسشی)"
+    
     elif text[0] == "he" or text[0] == "she" or text[0] == "i" or text[0] == "i'm" or text[0] == "you" or text[0] == "you're" or text[0] == "he's" or text[0] == "she's":
 
-        return "declarative"
+        return "declarative(خبری)"
+    
     else:
         
         return "please add a mark(?, !, .)"
+    
         
 def starting(text):
-    return last_points(split(text))
-
-
+    global txt
+    txt = text
+    return find(split(text))
